bbf9413e1faaa3daf6aceeee4b4cb13cbff2daaf
2202611 <132232633+2202611@users.noreply.github.com>
2024-11-27 20:55:21 +1200

    Update ybafarmhopper.lua

--- ybafarmhopper.lua
+++ ybafarmhopper.lua
@@ -279,7 +279,7 @@ while boolean do
                             end
                             TextLabel.Text = "picking up and selling item.."
                             task.wait(0.4)
-			                if not p:IsDescendantOf(workspace) then
+			                if p and not p:IsDescendantOf(workspace) then
                                 break
                             end
                         end
